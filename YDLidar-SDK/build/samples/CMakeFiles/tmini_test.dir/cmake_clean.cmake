325eac5c894b5ae66a861240ce93f178b01395c0
jeonsion <a24349663@gmail.com>
2023-10-20 21:04:36 +0900

    Recovery without .gif in each file

---
+++ YDLidar-SDK/build/samples/CMakeFiles/tmini_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../tmini_test"
+  "../tmini_test.pdb"
+  "CMakeFiles/tmini_test.dir/tmini_test.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tmini_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
